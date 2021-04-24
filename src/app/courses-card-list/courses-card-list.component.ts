b5c30e904f7c6ab39c377de464cb6544604a7e5b
Jared Hodgkins <phusiondev@gmail.com>
2021-04-24 12:09:01 -0400

    10. Data Modification Example in Reactive Style (Stateless)

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,7 +1,8 @@
 import { CourseDialogComponent } from "./../course-dialog/course-dialog.component";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, EventEmitter, Output } from "@angular/core";
 import { Course } from "../model/course";
+import { tap, filter } from "rxjs/operators";
 
 @Component({
   selector: "courses-card-list",
@@ -11,6 +12,9 @@ import { Course } from "../model/course";
 export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[] = [];
+
+  @Output()
+  private coursesChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) {}
 
@@ -26,5 +30,13 @@ export class CoursesCardListComponent implements OnInit {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe();
   }
 }
