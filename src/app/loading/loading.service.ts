d415e9d232d5f5964a67323b337199b6df4f9397
Jared Hodgkins <phusiondev@gmail.com>
2021-04-24 14:11:14 -0400

    13. Loading Service Reactive API Design

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,4 +1,15 @@
+import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 
 @Injectable()
-export class LoadingService {}
+export class LoadingService {
+  loading$: Observable<boolean>;
+
+  showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+    return undefined;
+  }
+
+  loadingOn() {}
+
+  loadingOff() {}
+}
