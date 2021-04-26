e5b1d6ade0a08a3f4dd3ad350a32f87c15603266
Jared Hodgkins <phusiondev@gmail.com>
2021-04-25 21:39:18 -0400

    26. Authentication State Management - Section Introduction

---
+++ src/app/services/auth.store.ts
@@ -0,0 +1,16 @@
+import { User } from "./../model/user";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthStore {
+  user$: Observable<User>;
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+
+  login(email: string, password: string): Observable<User> {}
+
+  logout() {}
+}
