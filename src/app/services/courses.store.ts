b18920cafecda7fb35d9aec512c320cdf25e819d
Jared Hodgkins <phusiondev@gmail.com>
2021-04-25 21:11:46 -0400

    25. Store Optimistic Data Modifications - Step-By-Step Implementation

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,7 +1,7 @@
 import { MessagesService } from "./../messages/messages.service";
 import { LoadingService } from "./../loading/loading.service";
 import { HttpClient } from "@angular/common/http";
-import { map, catchError, tap } from "rxjs/operators";
+import { map, catchError, tap, shareReplay } from "rxjs/operators";
 import { BehaviorSubject, Observable, throwError } from "rxjs";
 import { Injectable } from "@angular/core";
 import { Course, sortCoursesBySeqNo } from "../model/course";
@@ -16,7 +16,7 @@ export class CoursesStore {
   constructor(
     private http: HttpClient,
     private loading: LoadingService,
-    private messages: MessagesService
+    private messagesService: MessagesService
   ) {
     this.loadAllCourses();
   }
@@ -26,7 +26,7 @@ export class CoursesStore {
       map((response) => response["payload"]),
       catchError((err) => {
         const message = "Could not load courses";
-        this.messages.showErrors(message);
+        this.messagesService.showErrors(message);
         console.log(message, err);
         return throwError(err);
       }),
@@ -36,7 +36,31 @@ export class CoursesStore {
     this.loading.showLoaderUntilCompleted(loadCourses$).subscribe();
   }
 
-  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {}
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    const courses = this.subject.getValue();
+
+    const index = courses.findIndex((course) => course.id == courseId);
+
+    const newCourse: Course = {
+      ...courses[index],
+      ...changes,
+    };
+
+    const newCourses: Course[] = courses.slice(0);
+    newCourses[index] = newCourse;
+
+    this.subject.next(newCourses);
+
+    return this.http.put(`/api/courses/${courseId}`, changes).pipe(
+      catchError((err) => {
+        const message = "Could not save course";
+        console.log(message, err);
+        this.messagesService.showErrors(message);
+        return throwError(err);
+      }),
+      shareReplay()
+    );
+  }
 
   filterByCategory(category: string): Observable<Course[]> {
     return this.courses$.pipe(
